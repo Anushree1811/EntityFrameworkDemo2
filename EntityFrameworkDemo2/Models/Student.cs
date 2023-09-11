65f4689b2e79a1ec820d641fddddb81163509282
Vishnu Kumar <46111882+vishnukumarps@users.noreply.github.com>
2023-09-11 06:17:44 +0530

    Update Student.cs

--- EntityFrameworkDemo2/Models/Student.cs
+++ EntityFrameworkDemo2/Models/Student.cs
@@ -71,3 +71,23 @@ public class StudentViewModel
 
     public string? LastName { get; set; }
 }
+
+/* 
+ * Student class:
+ * 
+ * Address property: This represents a 1:1 relationship. Each student has one address.
+ * MarkList property: This represents a 1 to Many relationship. Each student can have multiple marks.
+ * TeamList property: This represents a Many to Many relationship. Each student can belong to multiple teams, and each team can have multiple students.
+ * 
+ * Team class:
+ * 
+ * StudentList property: This represents a Many to Many relationship. Each team can have multiple students, and each student can belong to multiple teams.
+ * 
+ * So, overall, your comments are correct in describing the relationships between the classes.\
+ * However, it's important to note that in a relational database, Many-to-Many relationships are typically implemented using a
+ * junction table (also known as an associative or linking table) to avoid direct Many-to-Many relationships. 
+ * You may need to configure such tables if you are working with a relational database like SQL Server or PostgreSQL.
+ * 
+ * Additionally, the use of nullable reference types (?) in your code indicates that some properties can be null, which is fine depending on your data model and business logic.
+ */
+
